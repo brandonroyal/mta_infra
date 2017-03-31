bab918c51ae0dbb51d5ef81b4ea89deef7de24ab
BrandonRoyal <BMRoyal@gmail.com>
2017-03-30 19:32:37 -0700

    added test with login

--- tests/azure/testdeploy.sh
+++ tests/azure/testdeploy.sh
@@ -1,1 +1,8 @@
 #/bin/bash
+az login  --service-principal -u $u -p $p --tenant $tenant
+
+export resource_group_name=broyal_ci$CIRCLE_BUILD_NUM
+export location=eastus
+export storage_account_name=broyalmta
+
+az group create --name $resource_group_name --location $location
