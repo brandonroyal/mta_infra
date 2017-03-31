31d59bfbda98939addc14eae8953188217446a57
BrandonRoyal <BMRoyal@gmail.com>
2017-03-31 16:16:21 -0700

    fixed extra escaped quotes 2

--- tests/azure/testdeploy.sh
+++ tests/azure/testdeploy.sh
@@ -11,7 +11,7 @@ az group create --name $resource_group_name --location $location
 
 #get azuredeploy.parameters.json and add STORAGE_ACCOUNT_KEY
 parameters="
-\"{
+\"\{
     \"prefix\": {
         \"value\": \"broyal\"
     },
@@ -42,7 +42,7 @@ parameters="
     \"storageContainerName\": {
         \"value\": \"hotfix\"
     }
-}\"
+\}\"
 "
 
 az group deployment create --template-file ./azure/azuredeploy.json --parameters $parameters -g $resource_group_name
