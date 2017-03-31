1971259a64867fd418a8fb636da69485f4bbce51
BrandonRoyal <BMRoyal@gmail.com>
2017-03-31 09:02:45 -0700

    testing paths 2

--- tests/azure/testdeploy.sh
+++ tests/azure/testdeploy.sh
@@ -8,7 +8,7 @@ export storage_account_name=broyalmta
 az group create --name $resource_group_name --location $location
 
 #sed "s/{{storageAccountKey}}/${STORAGE_ACCOUNT_KEY}/g" azuredeploy.parameters.json > azuredeploy.parameters2.json
-cat azuredeploy.parameters2.json
+cat azuredeploy.parameters.json
 
 #cleanup
 az group delete -n $resource_group_name -y
