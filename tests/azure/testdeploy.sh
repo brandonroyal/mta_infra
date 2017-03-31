dece9195c231b5c5bafe6d8f094accbea8a3d138
BrandonRoyal <BMRoyal@gmail.com>
2017-03-31 12:55:22 -0700

    replace test

--- tests/azure/testdeploy.sh
+++ tests/azure/testdeploy.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-az login  --service-principal -u $u -p $p --tenant $tenant
+# az login  --service-principal -u $u -p $p --tenant $tenant
 
-export resource_group_name=broyal_ci$CIRCLE_BUILD_NUM
-export location=eastus
-export storage_account_name=broyalmta
+# export resource_group_name=broyal_ci$CIRCLE_BUILD_NUM
+# export location=eastus
+# export storage_account_name=broyalmta
 
-az group create --name $resource_group_name --location $location
+# az group create --name $resource_group_name --location $location
 
 #sed "s/{{storageAccountKey}}/${STORAGE_ACCOUNT_KEY}/g" azuredeploy.parameters.json > azuredeploy.parameters2.json
 cat ./tests/azure/azuredeploy.template.json > azuredeploy.template.json
@@ -13,4 +13,4 @@ sed "s/{{storageAccountKey}}/${STORAGE_ACCOUNT_KEY}/g" azuredeploy.template.json
 cat azuredeploy.parameters.json
 
 #cleanup
-az group delete -n $resource_group_name -y
+# az group delete -n $resource_group_name -y
