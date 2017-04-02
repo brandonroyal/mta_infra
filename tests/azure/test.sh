56379947331970153179f073bf763a6893e0596a
BrandonRoyal <BMRoyal@gmail.com>
2017-04-02 08:43:07 -0700

    added forced exit

--- tests/azure/test.sh
+++ tests/azure/test.sh
@@ -52,7 +52,5 @@ echo "exit code: $?"
 az group deployment create --template-file ./azure/azuredeploy.json --parameters $parameters -g $resource_group_name
 
 #force exit code from deployment failure
-if [ $? -ne 0 ]
-then
-    exit $?
-fi
+echo "exit code: $?"
+exit $?
