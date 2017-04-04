34ae62c3d849329e9cd20bcebbd94287aed28ccc
BrandonRoyal <BMRoyal@gmail.com>
2017-04-03 21:19:30 -0700

    adding publish configs

--- build/publish_files.sh
+++ build/publish_files.sh
@@ -1,3 +1,9 @@
 #/bin/bash
 set -e
 
+#variables
+export resource_group_name="broyalmta"
+export storage_account_name="mtapoc"
+
+connectionString=$(az storage account show-connection-string --name $storage_account_name --resource-group $resource_group_name --key primary --query connectionString)
+az storage container create --name artifacts --public-access blob --connection-string $connectionString
