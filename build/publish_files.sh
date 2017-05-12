8e72427b98953b206ccf522f8c1019541dd49f5c
BrandonRoyal <BMRoyal@gmail.com>
2017-05-12 15:53:07 -0700

    added auto publish documentation

--- build/publish_files.sh
+++ build/publish_files.sh
@@ -8,7 +8,7 @@ git fetch --tags
 resource_group_name="broyal_ci"
 storage_account_name="mtapoc"
 tag=$(git describe --tags --abbrev=0)
-tag=$(echo $tag | sed 's/[^a-zA-Z0-9]//g')
+export tag=$(echo $tag | sed 's/[^a-zA-Z0-9]//g')
 
 #get storage account connection string
 connectionString=$(az storage account show-connection-string --name $storage_account_name --resource-group $resource_group_name --key primary --query connectionString)
@@ -41,3 +41,35 @@ done
 for filepath in ./docs/*; do
     upload_assets $filepath
 done
+
+#create new routes for documentation
+export APP_ROUTES="
+[
+    { 
+        \"redirect\": true, \"path\": \"/v2\", \"redirectPath\": \"/\"
+    },
+    {
+        \"title\": \"Modernizing Traditional Applications POC // Azure Deployment\",
+        \"path\": \"/\",
+        \"nextTitle\": \"Deploy Test App\",
+        \"nextPath\": \"/test-app\",
+        \"sourceHost\": \"mtapoc.blob.core.windows.net\",
+        \"sourcePath\": \"/$tag/mta_poc_azure_deployment.md\"
+    },
+    {
+        \"title\": \"Modernizing Traditional Applications POC // Deploy Test App\",
+        \"path\": \"/test-app\",
+        \"previousTitle\": \"Azure Infrastructure Deployment\",
+        \"previousPath\": \"/\",
+        \"sourceHost\": \"mtapoc.blob.core.windows.net\",
+        \"sourcePath\": \"/$tag/mta_poc_test_app.md\"
+    }
+]
+"
+
+# copy routes and update service
+echo $APP_ROUTES > routes.json
+scp routes.json docker@mta.dckr.org:/home/docker/routes.json
+ssh -T docker@mta.dckr.org 'bash -s' << 'ENDSSH'
+    docker service update --mount-add type=bind,source=`pwd`/routes.json,destination=/app/routes.json --image broyal/www-markdown:1.3 www
+ENDSSH
